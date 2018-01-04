e8fc0d044ee437f05c83fbacb3837086ff5321b8
Asif Rahman <asif0531@live.com>
2018-01-05 01:36:21 +0600

    added a vs2017 ng app for basic fundamental

---
+++ vs17/ClientApp/app/app.shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { CalculateComponent } from './components/calculator/calculate.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent, 
+        CalculateComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'calculator', component: CalculateComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
---
+++ vs17/ClientApp/app/components/calculator/calculate.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'calculator',
+    templateUrl: './calculate.component.html',
+    styleUrls: ['./calculate.component.css']
+})
+
+export class CalculateComponent {
+    public firstNumber: number;
+    public secondNumber: number;
+    public result: number;
+
+    public add() {
+        this.result = this.firstNumber + this.secondNumber;
+    }
+
+    public sub() {
+        this.result = this.firstNumber - this.secondNumber;
+    }
+
+    public mult() {
+        this.result = this.firstNumber * this.secondNumber;
+    }      
+
+}
